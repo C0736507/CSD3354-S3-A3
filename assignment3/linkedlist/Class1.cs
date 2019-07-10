89ce1134e58e117bd7639b00dd1f29b6ace91034
Gurpreet Kaur Randhawa <736507@student.tor.loc>
2019-07-10 12:00:26 -0400

    six commit

---
+++ assignment3/linkedlist/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist
+{
+    public class Class1
+    {
+    
+    }
+    class linkedlist
+    {
+        node head;
+    }
+    class node
+    {
+        public string studentname;
+        public node next;
+        public node prev;
+
+    }
+}
